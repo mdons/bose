2b02040a30172ae3ac59c10ec0881d92d354e5b3
mdons <michaeljdonnell@gmail.com>
2019-03-18 23:41:18 -0500

    completed up to route auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import "./App.css";
+
+const Home = () => (
+  <div>
+    <h2> Home </h2>
+  </div>
+);
+
+const City = () => (
+  <div>
+    <ul>
+      <li>San Francisco</li>
+      <li>Istanbul</li>
+      <li>Tokyo</li>
+    </ul>
+  </div>
+);
+
+const Courses = ({ match }) => (
+  <div>
+    <ul>
+      <li>
+        <Link to={`${match.url}/technology`}>Technology</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/business`}>Business</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/economics`}>Economics</Link>
+      </li>
+    </ul>
+
+    <Route
+      exact
+      path={`${match.path}/:course`}
+      render={({ match }) => <div> This is {match.params.course} </div>}
+    />
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li>
+            <Link to="/cities">Cities</Link>
+          </li>
+        </ul>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/courses" component={Courses} />
+        <Route path="/cities" component={City} />
       </div>
     );
   }
